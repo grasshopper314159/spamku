6ad6de4605d6684b9ca8c793a6c18b634c862914
Your Name <you@example.com>
2020-04-29 09:41:51 -0600

    sents

---
+++ h3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import nltk;
+nltk.download('punkt')
+
+ 	
+from nltk.corpus import PlaintextCorpusReader
+corpus_root = r'C:\cs\nlp\proj\corpus'
+poems = PlaintextCorpusReader(corpus_root, '.*')
+words=poems.words()
+sents=poems.sents()
+print(len(sents))
+print(sents[1])
+#wordlists.words('connectives')
+
